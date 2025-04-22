213eaf84336a7160e123044d511364143a32234b
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-04-22 09:51:12 +0530

    added logparser with parse line to extract data and load logs save data

--- app/parser/log_parser.py
+++ app/parser/log_parser.py
@@ -8,3 +8,27 @@ class LogEntry:
     message: str
     server: str
 
+
+class LogParser:
+
+    @staticmethod
+    def parse_line(line: str):
+        parts = line.strip().split()
+
+        return LogEntry(
+            timestamp=parts[0],
+            level=parts[1],
+            user=parts[2],
+            message=parts[3],
+            server=parts[4]
+        )
+
+    @staticmethod
+    def load_logs(filepath):
+        logs = []
+
+        with open(filepath, "r") as f:
+            for line in f:
+                logs.append(LogParser.parse_line(line))
+
+        return logs
